590b4c351e613040add62740dfdb980831045703
HashiniWithana <100822364+HashiniWithana@users.noreply.github.com>
2022-08-04 14:25:52 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,22 @@
+package com.rootcodelabs.java.SauceDemo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+}
